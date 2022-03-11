0747c14f477c3d28b883f0c16242d38a8bb9f7c7
dhavalsaresa25 <73735492+dhavalsaresa25@users.noreply.github.com>
2022-03-11 11:36:16 +0530

    relationship and model and  table

---
+++ database/migrations/2022_03_11_055150_create_billings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('agent_id')->unsigned()->nullable();
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
+            $table->string('weekly_billing',100)->nullable();
+            $table->string('average_close_out',100)->nullable();
+            $table->string('percentage',6)->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
