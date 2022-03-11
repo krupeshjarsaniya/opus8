1271486f9e30f4a3160b9d862831cb7dc049431d
dhavalsaresa25 <73735492+dhavalsaresa25@users.noreply.github.com>
2022-03-11 17:23:06 +0530

    psls crud

---
+++ resources/views/psls/js/psls-js.blade.php
@@ -0,0 +1,69 @@
+<script>
+    function pslsSubmit(ele)
+    {
+        var formId = $(ele).data('id');
+        // console.log(formId);
+        let myform = document.getElementById(formId);
+        let fd = new FormData(myform );
+
+        $.ajax({
+            url: "{{ route('psls.store') }}",
+            type: 'POST',
+            dataType: 'json',
+            data:fd,
+            cache: false,
+            contentType: false,
+            processData: false,
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+            },
+            beforeSend: function() {
+                $(".remedy-login-btn #load_more").text("loading");
+                $(".loader_area").addClass("show");
+            },
+            complete : function() {
+                $(".remedy-login-btn #load_more").text("Load More");
+                $(".loader_area").removeClass("show");
+            },
+            success: function(response, textStatus, jqXHR) {
+                if (jqXHR.status == 200) {
+                    toastr.success(response.message);
+                }
+            },
+            error: function(error) {
+                var message = null;
+                if (error.responseJSON) {
+                    if (typeof error.responseJSON.errors != "undefined") {
+                        $.each(error.responseJSON.errors, function(id, topic) {
+                            message = topic[0];
+                            return false;
+                        });
+                    }
+                    if (!message) {
+                        message = error.responseJSON.message;
+                    }
+                }
+                if (!message) {
+                    message = 'Something went wrong'
+                }
+                toastr.error(message);
+            },
+            complete: function(data) {
+
+                $(".loader_area").removeClass("show");
+
+                if (data.status == 401)
+                {
+                    $.each(data.error1, function (index, value) {
+                        $('.err_' + index+' input').addClass('border border-danger');
+                        $('.err_' + index).append('<span class="small alerts text-danger">' + value + '</span>');
+                    });
+                }
+
+                // location.reload()
+            },
+
+        });
+    }
+</script>
+
---
+++ resources/views/psls/psls.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+@section('title', 'Login')
+@section('body-class', 'hd-auth-body')
+@section('content')
+<section class="remedy-layout-wrapper">
+    <div class="container">
+        <h1 class="text-center font-weight-light">PSL<b>s</b></h1>
+            <div class="row mt-5">
+                <div class="col-lg-2"><h5>Agent</h5></div>
+                <div class="col-lg-10">
+                    <div class="row">
+                        <div class="col-lg-3"><h5>New on Board</h5></div>
+                        <div class="col-lg-3"><h5>Sector</h5></div>
+                        <div class="col-lg-3"><h5>Company</h5></div>
+                        <div class="col-lg-3"><h5>Action</h5></div>
+                    </div>
+                </div>
+            </div>
+
+            @foreach ($agents as $agent)
+                {{-- @dump($agent->psls->new_on_board) --}}
+                <div class="row border-top border-gray mt-5">
+                    <div class="col-lg-2 mt-2">
+                        <img src="{{ asset('assets/images/avatar-img.png') }}" alt="remedy" class="col-lg-2 rounded-circle p-0">
+                        <label><h5 class="pl-3"><a href="{{route('agent.chart',$agent)}}">{{$agent->first_name}}</a></h5></label>
+                    </div>
+
+                    <div class="col-lg-10 mt-2">
+                        <form action="#" onsubmit="return false" method="post" name="Form_Name_{{ $agent->first_name }}"  id="Form_Name_{{ $agent->first_name }}">
+                            <input type="hidden" value="{{$agent->id}}" name="agentId">
+                            <div class="row">
+                                <div class="col-lg-3">
+                                    <div class="remedy-input-icon-wrapper">
+                                        <input type="text" class="form-control " name="new_on_board" value="{{$agent->psls->new_on_board ?? ''}}" autofocus placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="remedy-input-icon-wrapper">
+                                        <input type="text" class="form-control " name="sector" value="{{$agent->psls->sector ??  ''}}" autofocus placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="remedy-input-icon-wrapper">
+                                        <input type="text" class=" form-control " name="company" value="{{$agent->psls->company ?? ''}}" autofocus placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    {{-- <a href="#" data-id="Form_Name_{{ $agent->first_name }}" onclick="SubmitBillings(this)" class="remedy-login-btn">Submit <i><img src="{{ asset('assets/images/back-arrow-icon.svg') }}" alt="remedy"></i></a> --}}
+                                    <button type="button" class="remedy-login-btn" data-id="Form_Name_{{ $agent->first_name }}" onclick="pslsSubmit(this)">Submit <i><img src="{{ asset('assets/images/back-arrow-icon.svg') }}" alt="remedy"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+
+            {{-- <div class="row border-top border-gray mt-4">
+                <div class="col-lg-2 mt-2">
+                     <img src="{{ asset('assets/images/avatar-img.png') }}" alt="remedy" class="col-lg-2 rounded-circle p-0">
+                    <label><h5 class="pl-3">Ben</h5></label>
+                </div>
+                <div class="col-lg-10 mt-2">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="remedy-input-icon-wrapper">
+                                    <input type="text" class=" form-control " name="" value="" autocomplete="" autofocus placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="remedy-input-icon-wrapper">
+                                    <input type="text" class=" form-control " name="" value="" autocomplete="" autofocus placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="remedy-input-icon-wrapper">
+                                    <input type="text" class=" form-control " name="" value="" autocomplete="" autofocus placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <button type="submit" class="remedy-login-btn">Submit <i><img src="{{ asset('assets/images/back-arrow-icon.svg') }}" alt="remedy"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+
+            {{-- <div class="form-btn-block">
+                <button type="submit" class="remedy-login-btn">Submit <i><img src="{{ asset('assets/images/back-arrow-icon.svg') }}" alt="remedy"></i></button>
+            </div> --}}
+    <div>
+</section>
+@endsection
+
+@push('js')
+    @include('psls.Js.psls-js')
+@endpush
